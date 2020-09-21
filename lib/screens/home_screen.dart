1d6c5974a7748aee218260e7fbf3df8b1e32ec32
Hoa <hoa@lodestonesolutions.com.au>
2020-09-21 16:23:48 +0700

    dx

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import '../components/MenuBottom.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,25 +8,126 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String dropdown = '';
+  bool switchValue = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
       backgroundColor: Colors.white,
       floatingActionButton: MenuBottom(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Home Screen'),
-            RaisedButton(
-              onPressed: () =>
-                  {Navigator.of(context).pushNamed('/choose-report')},
-              child: Text('Go to choose report'),
-            )
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                    child: Text(
+                  'Fisheries and Aquaculture',
+                  style: TextHeader,
+                )),
+                Center(
+                    child: Text(
+                  'Commercial Reporting',
+                  style: TextHeader,
+                )),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+
+                      Text('Licence No.'),
+                      DropdownButtonFormField(
+                        validator: (value) {
+
+                          if (value == null) {
+                            return 'Please enter Licence No.';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(8.0),
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.green, width: 2.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.green, width: 2.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.green, width: 2.0),
+                          ),
+                        ),
+                        items: <String>['A', 'B', 'C', 'D'].map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (v) =>
+                            setState(() => dropdown = v),
+                      ),
+                      SizedBox(
+                        height: 15,
+                        width: double.infinity,
+                      ),
+                      Text('PIN'),
+                      TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter Licence No.';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.all(8.0),
+                            border: OutlineInputBorder(
+                              borderSide:
+                              BorderSide(color: Colors.green, width: 2.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.green, width: 2.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.green, width: 2.0),
+                            )),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: RaisedButton(
+                          onPressed: () {
+                            // Validate returns true if the form is valid, or false
+                            // otherwise.
+                            if (_formKey.currentState.validate()) {
+                              // If the form is valid, display a Snackbar.
+                              print('OK');
+                            }
+                          },
+                          child: Text('Submit'),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+const TextHeader =
+    TextStyle(color: Colors.green, fontSize: 20, fontWeight: FontWeight.bold);
